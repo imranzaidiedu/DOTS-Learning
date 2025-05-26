e3e3e8f6d35ccaa0f0ffb20a410e02aab70a8ba3
Imran Bin Azhar <imran.zaidiedu@gmail.com>
2025-05-26 13:10:21 +0100

    Implemented rotation speed authoring for cube

---
+++ Assets/Scripts/HelloCube/Common/RotationSpeedAuthoring.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Entities;
+
+namespace HelloCube.Common
+{
+    public class RotationSpeedAuthoring : MonoBehaviour
+    {
+        public float rotationSpeed = 10f;
+
+        class Baker : Baker<RotationSpeedAuthoring>
+        {
+            public override void Bake(RotationSpeedAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new RotationSpeed()
+                {
+                    RadiansPerSecond = math.radians(authoring.rotationSpeed)
+                });
+            }
+        }
+    }
+
+    public struct RotationSpeed : IComponentData, IEnableableComponent
+    {
+        public float RadiansPerSecond;
+    }
+}
